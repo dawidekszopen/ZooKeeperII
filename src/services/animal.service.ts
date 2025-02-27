a152461d0684f9608c4938bef33de9d84132f223
dawidekszopen <62569747+dawidekszopen@users.noreply.github.com>
2025-02-27 14:29:45 +0100

    dodałem kilka funkcji do anial.service

---
+++ src/services/animal.service.ts
@@ -0,0 +1,66 @@
+import {Animal} from "../models/animal";
+
+class AnimalService{
+    private animals: Array<Animal> = [
+        {
+            id: 1,
+            name: "Simba",
+            species: "Lion",
+            age: 5,
+            isEndangered: true,
+            habitat: "Savanna"
+        },
+        {
+            id: 2,
+            name: "Nemo",
+            species: "Clownfish",
+            age: 2,
+            isEndangered: false,
+            habitat: "Ocean"
+        },
+    ]
+
+    GetAllAnimals(): Array<Animal>{
+        return this.animals
+    }
+
+    GetAllAnimalsEndagered(): Array<Animal>{
+        let EndageredAnimals: Array<Animal> = []
+
+        for(let i: number = 0; i < this.animals.length; i++){
+            if(this.animals[i]["isEndangered"]){
+                console.log(this.animals[i])
+                EndageredAnimals.push(this.animals[i])
+            }
+        }
+
+        return EndageredAnimals
+    }
+
+    GetAllAnimalsInHabitat(habitat: string): Array<Animal>{
+        let HabitatAnimals: Array<Animal> = [];
+
+        for(let i = 0; i < this.animals.length; i++){
+            if(this.animals[i]["habitat"] === habitat){
+                console.log(this.animals[i])
+                HabitatAnimals.push(this.animals[i])
+            }
+        }
+
+        return HabitatAnimals
+    }
+
+    GetAllAnimalsSpecies(species: string): Array<Animal>{
+        let SpeciesAnimals: Array<Animal> = [];
+
+
+        for(let i = 0; i < this.animals.length; i++){
+            if(this.animals[i]["species"] === species){
+                console.log(this.animals[i])
+                SpeciesAnimals.push(this.animals[i])
+            }
+        }
+
+        return SpeciesAnimals
+    }
+}
